bd16e4f8f372bc28b80398fc8716f10701649bf8
HumaAnwar <huma01122@gmail.com>
2024-06-29 12:22:32 -0700

    fragment home and drawer

--- app/src/main/java/com/hiskytechs/autocarehub/Ui/MainActivity.kt
+++ app/src/main/java/com/hiskytechs/autocarehub/Ui/MainActivity.kt
@@ -8,8 +8,10 @@ import com.hiskytechs.autocarehub.databinding.ActivityWorkShopAccess4Binding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding= ActivityMainBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
     }
 }
